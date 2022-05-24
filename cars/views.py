7f09f4991ea18936128017eccbbdc6f843eb7b78
unknown <tsrogers8@msn.com>
2022-05-24 09:06:22 -0600

    Created new view function fro getting cars by ID + Added new URLS path for car detail view function

--- cars/views.py
+++ cars/views.py
@@ -1,21 +1,24 @@
-from urllib import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Car
 from .serializers import CarSerializer
 
-@api_view(['GET', 'POST'])
-def cars_list (request):
 
+@api_view(['GET', 'POST'])
+def cars_list(request):
     if request.method == 'GET':
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
-
-
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def car_detail(request, pk):
+
+    print(pk)
+    return Response(pk)
